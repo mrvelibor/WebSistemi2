652f358e85d66ab0f86552a12436e960da741d48
mrvelibor <mr.velibor@gmail.com>
2017-01-27 19:19:39 +0100

    DZ11

---
+++ src/com/mrvelibor/websistemi2/controller/Keys/Views.java
@@ -0,0 +1,6 @@
+package com.mrvelibor.websistemi2.controller.Keys;
+
+public class Views {
+    public static final String DEFAULT_VIEW = "view";
+    public static final String ALTERNATIVE_VIEW = "alternativeView";
+}
